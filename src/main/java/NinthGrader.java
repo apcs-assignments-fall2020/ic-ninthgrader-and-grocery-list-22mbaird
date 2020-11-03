251571cd48f74d23acf8bbe72e49e32d0810b04f
22mbaird <22mbaird@athenian.org>
2020-11-03 16:41:42 +0000

    Finished code

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,20 @@
-public class NinthGrader {
-    
+public class NinthGrader extends Student {
+    public NinthGrader(String a, String b, int c) {
+        super(a, b, c);
+        
+    }
+    public NinthGrader() {
+        super("Wyatt", "Poage", 2024);
+        
+        
+    }
+    public void doSomething() {
+        System.out.println("Im doing ninth grade things");
+    }
+    public String toString(){
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.getClassYear()+ "(2024)";
+        return str;
+    }
+
+
 }
